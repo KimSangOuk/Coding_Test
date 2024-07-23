1f072ed87e389eb43601b46f4968e3b5fa6967af
KimSangOuk <kimsangouk1020@gmail.com>
2024-07-23 06:46:10 +0000

    baekjoon

---
+++ CodingTest/Coding_Test/BAEKJOON/11721.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s= sc.next();
+        for(int i=0;i<s.length();i+=10){
+            if(i+10<=s.length()){
+                System.out.println(s.substring(i,i+10));
+            }else{
+                System.out.println(s.substring(i));
+            }
+
+        }
+    }
+}
---
+++ CodingTest/Coding_Test/BAEKJOON/2442.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
